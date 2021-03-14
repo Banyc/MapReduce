1a3876e58580e118f4d71e6b5d5c3c26556afa0e
Banyc <36535895+Banyc@users.noreply.github.com>
2021-03-14 21:46:43 +0800

    [test] Master add test cases and program entry

--- src/MapReduce.Master/Program.cs
+++ src/MapReduce.Master/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using MapReduce.Master.Models;
 
 namespace MapReduce.Master
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var fileArray = Directory.GetFiles("test_inputs");
+
+            MasterSettings settings = new()
+            {
+                InputFilePaths = new List<string>(fileArray),
+                IpAddress = "localhost",
+                Port = 5000,
+                ReduceTaskCount = 6
+            };
+            Helpers.Master master = new(settings);
+
+            CancellationTokenSource cancelToken = new();
+
+            Task masterTask = master.StartAsync(cancelToken.Token);
+
+            Console.WriteLine("Press any key to stop master.");
+            Console.ReadKey();
+
+            cancelToken.Cancel();
+            Console.WriteLine("Killing master...");
+
+            try
+            {
+                await Task.WhenAll(masterTask).ConfigureAwait(false);
+            }
+            catch (TaskCanceledException) { }
         }
     }
 }
