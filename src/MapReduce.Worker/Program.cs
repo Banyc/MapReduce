3c8cce1b654ece929543c237f27da026d6c01ead
Banyc <36535895+Banyc@users.noreply.github.com>
2021-03-15 01:52:15 +0800

    [worker] Release resourses

--- src/MapReduce.Worker/Program.cs
+++ src/MapReduce.Worker/Program.cs
@@ -25,35 +25,45 @@ namespace MapReduce.Worker
             List<Helpers.Worker<string, int>> workers = new();
             List<Task> tasks = new();
 
-            int i;
-            for (i = 0; i < numWorkers; i++)
-            {
-                Helpers.Worker<string, int> worker = new(
-                    settings: new()
-                    {
-                        WorkerUuid = Guid.NewGuid().ToString(),
-                        MappedOutputDirectory = "mapped",
-                        ReducedOutputDirectory = "reduced",
-                    },
-                    rpcClientFactory: rpcClientFactory,
-                    mappingPhase: workCount,
-                    reducingPhase: workCount,
-                    partitioningPhase: defaultPartitioner
-                );
-
-                CancellationTokenSource cancelToken = new();
-
-                Task task = worker.StartAsync(cancelToken.Token);
-
-                workers.Add(worker);
-                tasks.Add(task);
-            }
-
             try
             {
-                await Task.WhenAll(tasks).ConfigureAwait(false);
+                int i;
+                for (i = 0; i < numWorkers; i++)
+                {
+                    Helpers.Worker<string, int> worker = new(
+                        settings: new()
+                        {
+                            WorkerUuid = Guid.NewGuid().ToString(),
+                            MappedOutputDirectory = "mapped",
+                            ReducedOutputDirectory = "reduced",
+                        },
+                        rpcClientFactory: rpcClientFactory,
+                        mappingPhase: workCount,
+                        reducingPhase: workCount,
+                        partitioningPhase: defaultPartitioner
+                    );
+
+                    CancellationTokenSource cancelToken = new();
+
+                    Task task = worker.StartAsync(cancelToken.Token);
+
+                    workers.Add(worker);
+                    tasks.Add(task);
+                }
+
+                try
+                {
+                    await Task.WhenAll(tasks).ConfigureAwait(false);
+                }
+                catch (TaskCanceledException) { }
             }
-            catch (TaskCanceledException) { }
+            finally
+            {
+                foreach (var worker in workers)
+                {
+                    worker.Dispose();
+                }
+            }
         }
     }
 }
